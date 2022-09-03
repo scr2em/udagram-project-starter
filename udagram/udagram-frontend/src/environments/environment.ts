8338593d6adac2633e4ad3a9d80b01914868e756
Mohamed M. Abdelgawad <mohammed.abdelgawad@leadliaison.com>
2022-09-03 16:45:57 +0200

    update frontend apiHost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-dimk6fbg.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://egfwdudagram-env.eba-en9aatmx.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
